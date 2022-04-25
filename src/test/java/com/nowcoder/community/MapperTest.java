1b73d33095451531c13692e933e2110c09f1ffef
tanhaojie <414704421@qq.com>
2022-04-25 22:50:59 +0800

    First commit

--- src/test/java/com/nowcoder/community/MapperTest.java
+++ src/test/java/com/nowcoder/community/MapperTest.java
@@ -3,9 +3,11 @@ package com.nowcoder.community;
 
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.dao.LoginTicketMapper;
+import com.nowcoder.community.dao.MessageMapper;
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.entity.LoginTicket;
+import com.nowcoder.community.entity.Message;
 import com.nowcoder.community.entity.User;
 
 import org.junit.Test;
@@ -31,6 +33,9 @@ public class MapperTest {
 
     @Autowired
     private LoginTicketMapper loginTciketMapper;
+
+    @Autowired
+    private MessageMapper messageMapper;
 
     @Test
     public void testSelectUser() {
@@ -120,6 +125,28 @@ public class MapperTest {
         discussPost.setUserId(0);
         discussPostMapper.insertDiscussPost(discussPost);
 
+    }
+
+    @Test
+    public void testMessage(){
+        List<Message> list=messageMapper.selectConversations(111,0,10);
+        for (Message message :list){
+            System.out.println(message);
+        }
+
+        int count = messageMapper.selectConversationCount(111);
+        System.out.println(count);
+
+        list = messageMapper.selectLetters("111_112",0,10);
+        for (Message message :list){
+            System.out.println(message);
+        }
+
+        count = messageMapper.selectLetterCount("111_112");
+        System.out.println(count);
+
+        count = messageMapper.selectLetterUnreadCount(131,"111_131");
+        System.out.println(count);
     }
 
 }
